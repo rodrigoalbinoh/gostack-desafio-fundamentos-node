6943ff37fda6664f7d0e213bbae8092ed4e531e5
Rodrigo <rodrigo.hammes7@gmail.com>
2020-04-18 15:36:17 -0300

    implements getBalance all and create methods

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,74 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income: number = this.transactions.reduce(
+      (accumulator, transaction) => {
+        const { type, value } = transaction;
+
+        if (type === 'income') {
+          // eslint-disable-next-line no-param-reassign
+          accumulator += value;
+        }
+
+        return accumulator;
+      },
+      0,
+    );
+
+    const outcome: number = this.transactions.reduce(
+      (accumulator, transaction) => {
+        const { type, value } = transaction;
+
+        if (type === 'outcome') {
+          // eslint-disable-next-line no-param-reassign
+          accumulator += value;
+        }
+
+        return accumulator;
+      },
+      0,
+    );
+
+    this.balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
